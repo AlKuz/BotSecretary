99b94a2df103d5a10f6dc7dc0f27bcfc589c5de6
AlKuz <a.v.kuznetsov31@gmail.com>
2023-06-19 16:48:02 +0300

    Added tests for prompt_generator

--- src/bot_secretary/prompt_generator.py
+++ src/bot_secretary/prompt_generator.py
@@ -1,44 +1,38 @@
 import re
 
-from jinja2 import Environment, PackageLoader, Template, select_autoescape
+from jinja2 import Environment, FileSystemLoader, Template, select_autoescape
 from pathlib import Path
-
-from typing import Iterator
 
 
 class Prompt:
 
     def __init__(self, template: Template, max_prompt_words: int, **kwargs):
         self.__template = template
-        self.__re = re.compile(r"\b(?:\w+(?:\W+|$)){1,%d}" % (max_prompt_words - 1))
+        self.__re = re.compile(r"\b(?:\w+(?:\W+|$)){1,%d}" % max_prompt_words)
         self.__kwargs = kwargs
 
     def render(self) -> str:
         return self.__template.render(**self.__kwargs)
 
     @property
-    def chunks(self) -> Iterator[str]:
-        for chunk in self.__re.findall(str(self)):
-            yield chunk
+    def chunks(self) -> list[str]:
+        return self.__re.findall(self.render())
 
 
 class PromptGenerator:
 
     def __init__(self, templates_folder: Path | None = None, max_prompt_words: int = 2500):
-        self.__templates_folder = templates_folder or Path(__file__).parent / "templates"
+        self.__templates_folder = templates_folder or Path(__file__).parent / "prompt_templates"
         self.__max_prompt_words = max_prompt_words
         self.__env = self.__get_environment()
 
     def __get_environment(self) -> Environment:
         return Environment(
-            loader=PackageLoader(
-                package_name="prompt_templates",
-                package_path=str(self.__templates_folder)
-            ),
+            loader=FileSystemLoader(str(self.__templates_folder)),
             autoescape=select_autoescape()
         )
 
-    def __load_template(self, name: str) -> Template:
+    def load_template(self, name: str) -> Template:
         path = self.__templates_folder / f"{name}.html"
         if not path.exists():
             raise FileExistsError(f"The template '{name}' doesn't exist in '{self.__templates_folder}'")
@@ -49,15 +43,15 @@ class PromptGenerator:
         return self.__templates_folder
 
     def dialog_settings(self) -> Prompt:
-        return Prompt(self.__load_template("dialog_settings"), self.__max_prompt_words)
+        return Prompt(self.load_template("dialog_settings"), self.__max_prompt_words)
 
     def background_prompt(self, background_path: str | Path) -> Prompt:
         with open(background_path, 'r') as file:
-            return Prompt(self.__load_template("background_prompt"), self.__max_prompt_words, text=file.read())
+            return Prompt(self.load_template("background_prompt"), self.__max_prompt_words, text=file.read())
 
     def style_prompt(self, style_path: str | Path) -> Prompt:
         with open(style_path, 'r') as file:
-            return Prompt(self.__load_template("style_prompt"), self.__max_prompt_words, text=file.read())
+            return Prompt(self.load_template("style_prompt"), self.__max_prompt_words, text=file.read())
 
     def message_prompt(self, text: str) -> Prompt:
-        return Prompt(self.__load_template("message_prompt"), self.__max_prompt_words, text=text)
+        return Prompt(self.load_template("message_prompt"), self.__max_prompt_words, text=text)
